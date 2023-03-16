5ded92876930e1db2cff5a96b6f7cb48862f346d
Tejas Udupa <tejasudupa1285@gmail.com>
2023-03-16 23:28:10 +0530

    Add `Flag` helper classes
    Signed-off-by: Tejas Udupa <tejasudupa1285@gmail.com>

---
+++ include/flag.h
@@ -0,0 +1,33 @@
+#ifndef FLAG_H
+#define FLAG_H
+
+class Flag{
+        std::uint8_t flag;
+    public:
+        void initFlag(bool);
+        void setFlag();
+        void resetFlag();
+        bool isFlagSet();
+};
+inline void Flag::initFlag(bool state = true)
+{
+    if(state)
+        flag = 1;
+    else
+        flag = 0;
+}
+inline void Flag::setFlag(){
+    flag = 1;
+}
+
+inline void Flag::resetFlag(){
+    flag = 0;
+}
+
+inline bool Flag::isFlagSet(){
+    if(flag)
+        return true;
+    return false;
+}
+
+#endif
