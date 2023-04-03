b3a81611efe3f06677a5a50c496e4fb42d8fbf86
Tejas Udupa <tejasudupa1285@gmail.com>
2023-04-03 23:07:56 +0530

    packet.hpp: add server header codes
    Signed-off-by: Tejas Udupa <tejasudupa1285@gmail.com>

--- include/packet.hpp
+++ include/packet.hpp
@@ -1,7 +1,7 @@
 #ifndef PACKET_H
 #define PACKET_H
 
-enum computeUnitPacketStatus {
+enum ComputeUnitPacketStatus {
     P_RESET     = 1 << 0, //Empty packet essentially
     P_HANDSHAKE = 1 << 1, //Handshake snd,ack packet
     P_DATA_ACK  = 1 << 2, //Table received ack
@@ -11,4 +11,14 @@ enum computeUnitPacketStatus {
     P_QSEND     = 1 << 6, //Client is in quick send mode
     P_SEIZE     = 1 << 7, //Client has seized receiving packets
 };
+
+enum ServerPacketCodes {
+    SP_HANDSHAKE    = 1 << 0,   //Server handshake packet
+    SP_DATA_SENT    = 1 << 1,   //Server sent csv data
+    SP_INTR_ACK     = 1 << 2,   //Server received the intermediate data
+    SP_FRES_ACK     = 1 << 3,   //Server received the result(includes res tableID)
+    SP_ERR          = 1 << 4,   //Server received packet was corrupt 
+    SP_IGNORE       = 1 << 5,   //Status is ignored
+}
+
 #endif
