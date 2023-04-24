ad3fb4b43e95764913aa7234a640c341445dab5c
Tejas Udupa <tejasudupa1285@gmail.com>
2023-04-24 22:36:57 +0530

    packet_processor: Fix compiler error and return json packet
    Signed-off-by: Tejas Udupa <tejasudupa1285@gmail.com>

--- packet_processor/packet_constructor.cpp
+++ packet_processor/packet_constructor.cpp
@@ -1,7 +1,9 @@
 #include "packet_constructor.hpp"
 
-json PacketConstructor::create(ServerPacketCodes code, std::uint64_t workerUid, json packet = json({}))
+json PacketConstructor::create(ServerPacketCodes code, std::uint64_t workerUid, json packet)
 {
     packet["head"] = code;
     packet["id"] = workerUid;
+
+    return packet;
 }
