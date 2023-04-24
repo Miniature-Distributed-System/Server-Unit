64312db672834729c321f3dd1bfc4b2cf21909d6
Tejas Udupa <tejasudupa1285@gmail.com>
2023-04-24 22:35:17 +0530

    packet_processor: Guard json libs from cyclic include
    Signed-off-by: Tejas Udupa <tejasudupa1285@gmail.com>

--- packet_processor/packet_constructor.hpp
+++ packet_processor/packet_constructor.hpp
@@ -1,11 +1,13 @@
 #ifndef PKT_CREATOR_H
 #define PKT_CREATOR_H
 
-#include "../lib/nlohmann/json-schema.hpp"
 #include "../include/packet.hpp"
 
-using nlohmann::json_schema::json_validator;
+#ifndef JSON_H
+#define JSON_H
+#include "../lib/nlohmann/json-schema.hpp"
 using json = nlohmann::json;
+#endif
 
 class PacketConstructor {
     public:
