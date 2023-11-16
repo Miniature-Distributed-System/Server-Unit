1decc348f288f2e7f76b9e1d3e3cfe92a44682a1
Tejas Udupa <tejasudupa1285@gmail.com>
2023-11-16 10:57:17 +0530

    sender: Fix `findIdealWorker` method call

--- sender_unit/sender_core.cpp
+++ sender_unit/sender_core.cpp
@@ -150,7 +150,7 @@ void pushUserDataToWorkerQueue()
     UserDataTable* userData;
     json packet;
 
-    worker = findIdealWorker();
+    worker = findIdealWorker(globalSenderSink->getTopTaskPriority());
     if(worker == NULL){
         return;
     }
